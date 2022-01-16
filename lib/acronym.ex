32a7391f5ae2bced36e42989af792ed3d8eefc54
Gerald Annan <annang2@stanbic.com.gh>
2022-01-16 23:52:41 +0000

    adds doctest abbreviate function

--- lib/acronym.ex
+++ lib/acronym.ex
@@ -2,6 +2,10 @@ defmodule Acronym do
   @doc """
   Generate an acronym from a string.
   "This is a string" => "TIAS"
+
+  ## Examples
+    iex> Acronym.abbreviate("Something - I made up from thin air")
+    "SIMUFTA"
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
